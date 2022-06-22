775afbebd0995609b91af8a599b4f3f6bdd604bf
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-06-22 21:14:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC827unf2Z2YxUCijcCSmgIN4Vp0payzR8",
-    authDomain: "spty-cl.firebaseapp.com",
-    projectId: "spty-cl",
-    storageBucket: "spty-cl.appspot.com",
-    messagingSenderId: "845739232949",
-    appId: "1:845739232949:web:9fc8b031015a9330ea78fc",
-    measurementId: "G-8MZ27G3K78"
+Enter Yours
 };
 
 // Initialize Firebase
